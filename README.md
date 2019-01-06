a6f33da4f0f37d05ca3e362ed4d68f46bae44c81
gjf <gjf.fixxxer@gmail.com>
2019-01-07 00:13:32 +0300

    Added interactivity
    Now it is possible to choose SBC bitrate during installation

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 Bluetooth stack bit rate changer + aptX & aptX-HD Enabler Module
 Possibly some files are not necessary, but who cares )
-!!! Tested on Redmi 4X only !!!
+Tested on Redmi 4X.
 
 ## Requirements ##
-- Redmi 4x. You can test it on your device and let me know does it work. No guarantee!
-- MIUI 10
+- Redmi 4x??? Try it and let me know.
+- MIUI 10 or any Android >=7
 
 BT stack is modified in the way so it uses:
 - Dual Channel for any audio connection
-- Maximum bitrate depending on GitHub branch used. Test it on your headphones. Normally 484 kbit/s is OK with any headphones, personally I use 576 kbit/s without any problems.
+- Maximum bitrate depending on GitHub branch used. Test it on your headphones. Normally 454 kbit/s is OK with any headphones, personally I use 576 kbit/s without any problems.
 
 
--- config.sh
+++ config.sh
@@ -45,7 +45,7 @@ print_modname() {
   ui_print "****aptX & aptX-HD enabled*****"
   ui_print "*           by @gjf           *"
   ui_print "*******************************"
-  ui_print "***!TESTED ON REDMI 4X ONLY!***"  
+  ui_print "*Note: TESTED ON REDMI 4X ONLY*"  
   ui_print "*******************************"
 }
 
@@ -101,3 +101,94 @@ set_permissions() {
 # difficult for you to migrate your modules to newer template versions.
 # Make update-binary as clean as possible, try to only do function calls in it.
 
+keytest() {
+  ui_print " - Vol Key Test -"
+  ui_print "   Press Vol Up:"
+  (/system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events) || return 1
+  return 0
+}
+
+chooseport() {
+  #note from chainfire @xda-developers: getevent behaves weird when piped, and busybox grep likes that even less than toolbox/toybox grep
+  while true; do
+    /system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events
+    if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUME >/dev/null`); then
+      break
+    fi
+  done
+  if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUMEUP >/dev/null`); then
+    return 1
+  else
+    return 0
+  fi
+}
+
+chooseportold() {
+  # Calling it first time detects previous input. Calling it second time will do what we want
+  $KEYCHECK
+  $KEYCHECK
+  SEL=$?
+  if [ "$1" == "UP" ]; then
+    UP=$SEL
+  elif [ "$1" == "DOWN" ]; then
+    DOWN=$SEL
+  elif [ $SEL -eq $UP ]; then
+    return 1
+  elif [ $SEL -eq $DOWN ]; then
+    return 0
+  else
+    ui_print "   Vol key not detected!"
+    abort "   Sorry for that."
+  fi
+}
+
+choosebitrate() {
+  # Keycheck binary by someone755 @Github, idea for code below by Zappo @xda-developers
+  KEYCHECK=$INSTALLER/common/keycheck
+  chmod 755 $KEYCHECK
+
+  if keytest; then
+    FUNCTION=chooseport
+  else
+    FUNCTION=chooseportold
+    ui_print "   ! Legacy device detected! Using old keycheck method"
+    ui_print " "
+    ui_print "- Vol Key Programming -"
+    ui_print "   Press Vol Up Again:"
+    $FUNCTION "UP"
+    ui_print "   Press Vol Down"
+    $FUNCTION "DOWN"
+  fi
+    BITRATE=0
+	while [ $BITRATE -eq 0 ]; do
+		ui_print " "
+		ui_print " - Select Bitrate for SBC codec -"
+		ui_print "   Choose which bitrate you want to install:"
+		ui_print "   Vol- = 454 kbit/s (works in most cases), Vol+ = higher"
+		if $FUNCTION; then BITRATE=454
+			else 
+				ui_print " "
+				ui_print "   Vol- = 482 kbit/s, Vol+ = higher"
+				if $FUNCTION; then BITRATE=482
+					else 
+						ui_print " "
+						ui_print "   Vol- = 486 kbit/s, Vol+ = higher"
+						if $FUNCTION; then BITRATE=486
+							else
+								ui_print " "
+								ui_print "   Vol- = 576 kbit/s, Vol+ = higher"
+								if $FUNCTION; then BITRATE=576
+									else
+										ui_print " "
+										ui_print "   Vol- = 800 kbit/s (not recommended), Vol+ = start from the beginning"
+										if $FUNCTION; then BITRATE=800
+										fi
+								fi
+						fi
+				fi
+		fi
+	done
+  ui_print "   You have chosen $BITRATE kbit/s"
+  cp -afr $MODPATH/system/lib/hw/BITRATES/$(echo $BITRATE)/bluetooth.default.so $MODPATH/system/lib/hw/bluetooth.default.so
+  rm -rf $MODPATH/system/lib/hw/BITRATES
+}
